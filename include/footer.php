2814ade552565d75fc19e2c9bed48ccf161a69d4
vthil3049 <vani.thilagar@gmail.com>
2016-02-01 15:26:05 -0500

    Modified footer copyright message

--- include/footer.php
+++ include/footer.php
@@ -1,6 +1,6 @@
 <div class="panel-footer">
 <div class="row">
-		<p class="text-center text-info">Copyright &copy; Neha Parikh</p>
+		<p class="text-center text-info">&copy; 2011-<?php echo date('Y') ?> Neha Parikh</p>
 </div>
 </div>
 
