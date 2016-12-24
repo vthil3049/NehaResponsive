d7b9b27461f5ce5fe35b322b3fed98121c87a1f9
Vani Thilagar <vthilagar@g.harvard.edu>
2016-12-24 12:35:54 -0500

    Updated index page based on Neha's discussions on 12/22

--- index.php
+++ index.php
@@ -1,45 +1,69 @@
 <!DOCTYPE html>
 <html>
-	<head>
-		<title>Chidambaram Nritya Kala Academy</title>
-		<?php include("include/htmlhead.php"); ?>
-	</head>
-	<body id="home" class="container-fluid">
-			<?php include("include/header.php"); ?>
-			<?php include("include/navigation.php"); ?>
-			<div class="container-fluid">
-					<div id="main" class="row">
-							<div class="col-md-2">
-								<h4 class="text-info text-center">Neha's students win awards in dance competions every year</h4>
-								<h4 class="text-warning text-center"><a class="btn btn-primary" href="Awards.php"><i class="fa fa-trophy fa-4x" style="color:gold"aria-hidden="true"></i></a></h4>
-							</div>
-							<div class="col-md-4" ><h3 class="text-info text-center">Welcome to Neha's dance school</h3>
-									<h4 class="text-info text-center">Indian Classical Dance instruction by a talented teacher, performer and an expert choreographer!<br />
-									Serving Greater Nashua, New Hampshire since 2005...<br /><br /></h4>
-									<p>Specialized in the highly evolved and stylized ancient arts of <strong><span class="text-success"> <a href="TheArts.php#Bharatanatyam">Bharata Natyam</a>, <a href="TheArts.php#Mohiniattam">Mohini Attam</a> and traditional charming folk dances of India.</span></strong> </p><br />
-									<p>Also experienced in teaching <strong><span class="text-success">Bollywood dance groups</span></strong>.<br />
-									Available to choreograph group or individual lessons.<br /> </p>
-									<h4>Specializes in teaching children, but students of all ages are welcome</h4>
-									<h3 class="text-info text-center">Neha's students have regularly won competitions in classical, folk and Bollywood dance competitions</h3>
-							<p>Would you like to try out a class?<br />
+<head>
+	<title>Chidambaram Nritya Kala Academy</title>
+	<?php include("include/htmlhead.php"); ?>
+</head>
+<body id="home" class="container-fluid">
+	<?php include("include/header.php"); ?>
+	<?php include("include/navigation.php"); ?>
+	<div class="container-fluid">
+		<div id="main" class="row">
+			<div class="col-md-2">
+				<h4 class="text-info text-center">Neha's students win awards in dance competions every year</h4>
+				<h4 class="text-warning text-center"><a class="btn btn-primary" href="Awards.php"><i class="fa fa-trophy fa-4x" style="color:gold" aria-hidden="true"></i></a></h4>
+				<div>
+					<p>
+						&nbsp;
+					</p>
+					<p>Would you like to try out a class?<br />
 						Contact Neha today!<br />
-						<i class="fa fa-envelope-o"></i>&nbsp;&nbsp;<a href="mailto:chidambaramdance@gmail.com">Neha Parikh</a><br/> 
+						<i class="fa fa-envelope-o"></i>&nbsp;&nbsp;<a href="mailto:chidambaramdance@gmail.com">Neha Parikh</a><br/>
 						<i class="fa fa-phone"></i>&nbsp;&nbsp;(603)546-5480<br/><br/>
 						<i class="fa fa-clock-o"></i>&nbsp;&nbsp;Class Timings:<br/>
 						<h4 class="text-info">Weekdays (Mon-Fri): 4:30pm - 7:30pm<br/>
-						Weekends: Saturday : 10am to 12 noon.<br/><br/>
-						Saturday afternoon classes available by prescheduled appointment.<br/>
+							Weekends: Saturday : 10am to 12 noon.<br/><br/>
+							Saturday afternoon classes available by prescheduled appointment.<br/>
 						</h4>
 						<p><i class="fa fa-map-marker"></i>&nbsp;&nbsp;
-						<a href="https://goo.gl/maps/TJpZj">Address:491, Amherst Street Unit 14, Nashua NH 03063</a></p>		
-							</div>
-							<div class="col-md-4">
-									<p>
-											<img title="Neha Parikh- Classical Dancer, teacher, choreographer" alt="Neha Parikh, teacher, choreographer" src="images/PhotoShoot_1_web.jpg" class="img-responsive" />
-									</p>
-							</div>
+							<a href="https://goo.gl/maps/TJpZj">Address:491, Amherst Street Unit 14, Nashua NH 03063</a></p>
+						</div>
 					</div>
-			</div>
-			<?php include("include/footer.php"); ?>
-	</body>
-</html>
+					<div class="col-md-5" ><h3 class="text-info text-center">Welcome to Neha's dance school</h3>
+						<h4 class="text-info text-center">Indian Classical Dance instruction by a talented teacher, performer and an expert choreographer!<br />
+							Serving Greater Nashua, New Hampshire since 2005...<br /><br /></h4>
+							<p>Specialized in the highly evolved and stylized ancient arts of <strong><span class="text-success"> <a href="TheArts.php#Bharatanatyam">Bharata Natyam</a>, <a href="TheArts.php#Mohiniattam">Mohini Attam</a> and traditional charming folk dances of India.</span></strong> </p><br />
+							<p>Also experienced in teaching <strong><span class="text-success">Bollywood dance groups</span></strong>.<br />
+								Available to choreograph group or individual lessons.<br /> </p>
+								<h4>Specializes in teaching children, but students of all ages are welcome</h4>
+								<div class="emboss">
+									<h4 class="text-success text-center">Why Learn Classical Indian Dance?</h4>
+									<ol>
+										<li>Learning classical dances makes you a BEAUTIFUL , GRACEFUL and ELEGANT person inside out. It helps tone your body and gives you a
+											beautiful posture and a healthy glowing skin. It enchances your facial features as well.</li>
+											<li>Improves flexibility and stamina.</li>
+											<li>Boosts your physical endurance, self confidence and self esteem.</li>
+											<li>Improves attention span and memory of kids with quick reflexes. Helps you with better coordination and makes you dexterous.</li>
+											<li>The Indian classical dance technique and methodology gives you an inward gaze and thereby improves focus, concentration and self awareness.</li>
+											<li>Teaches you self discipline, perseverance and responsibility.</li>
+											<li>Brings about a good balance and harmony of your right and left brain.</li>
+											<li>Helps you with inner peace and calmness.</li>
+											<li>A deeper connection with the Indian cultural roots and an insight into the rich Indian mythology and the Hindu religion.</li>
+											<li>Classical Indian dance and traditional Indian culture has such great DEPTH, that it can bring about a great change in your child's development at school, other actiivites and over all personality.</li>
+										</ol>
+
+									</div>
+									<p>
+										&nbsp;
+									</p>
+								</div>
+								<div class="col-md-3">
+									<p>
+										<img title="Neha Parikh- Classical Dancer, teacher, choreographer" alt="Neha Parikh, teacher, choreographer" src="images/PhotoShoot_1_web.jpg" class="img-responsive" />
+									</p>
+								</div>
+							</div>
+						</div>
+						<?php include("include/footer.php"); ?>
+					</body>
+					</html>
