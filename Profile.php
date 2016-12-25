77c06d38ccb61e73555dd4e3cb5879ad396c6a69
Vani Thilagar <vthilagar@g.harvard.edu>
2016-12-25 09:24:00 -0500

    Updated the profile page with more images

--- Profile.php
+++ Profile.php
@@ -14,26 +14,29 @@
                 This internationally famous institution was founded by Padmashri Dr. Kanak Rele.
                 Neha completed her Bachelor's and Master's in fine arts (Classical dance) from Nalanda specializing in <strong class="text-info">Bharatanatyam.</strong> She is also a trained artiste in <strong class="text-info">Mohiniattam.</strong><br/><br/>
 				<img src="images/Arekar.jpg" alt="image of Vaibhav Arekar, Neha's guru" title="Shri. Vaibhav Arekar" id="ArekarImg"/>Neha was fortunate enough to be guided by very eminent Gurus like Smt. Thangamani Nagarajan, Smt.Malati Agniswaran, <a href="http://www.vaibhavarekar.com/">Shri Vaibhav Arekar</a> and Smt.Ambika Vishwanathan who were the lecturers at Nalanda Institute.<br /><br />
-                She also had the honor of performing major roles in the most prestigious productions of Nalanda like  'Shilappadikaram', 'Santavani', 'Kanchanmrig', and 'Swapnavasadattam' at various prominent venues in India. At Nalanda as a student she was trained to choreograph dance ballets and also train other students during this period. 
+                She also had the honor of performing major roles in the most prestigious productions of Nalanda like  'Shilappadikaram', 'Santavani', 'Kanchanmrig', and 'Swapnavasadattam' at various prominent venues in India. At Nalanda as a student she was trained to choreograph dance ballets and also train other students during this period.
                 </p>
                 <p>
                     Since 2005, when she relocated to the US, she has been teaching students in the US and is well known for her adherence to strict standards and yet very creative performances.  Following the Guru-Shishya traditions in teaching, she interacts with students in trying to get the best out of every student. <br />
                     It is to her credit that many of her students have performed their 'Arangetram' or solo dance debut recitals since 2006 and more are on their way to achieving this milestone.<br />
                 </p>
                 <p>
-        Neha is an active member of India Association of New Hampshire since 2005 which awarded her with <strong class="text-info">IANH "Excellence Award" in April, 2011.</strong> She was also presented the <strong class="text-info">"Spotlight on Excellence Award" in the category of "Dedicated dancer"</strong> by Lokvani in September 2011. 
+        Neha is an active member of India Association of New Hampshire since 2005 which awarded her with <strong class="text-info">IANH "Excellence Award" in April, 2011.</strong> She was also presented the <strong class="text-info">"Spotlight on Excellence Award" in the category of "Dedicated dancer"</strong> by Lokvani in September 2011.
                 </p>
                 <p><img title="Neha receives an excellence award" alt="Neha receiving excellence award" src="images/ExcellenceAward.jpg" class="img-responsive center-block"/></p>
-                <div> <p class="text-info text-center">Neha receiving excellence award.</p></div>			
+                <div> <p class="text-info text-center">Neha receiving excellence award.</p></div>
             </div>
-            <div class="col-md-5 ">
+            <div class="col-md-6 ">
                 <p>
-                    <img title="Neha during her arangetram" alt="Neha during her arangetram" src="images/Neha600_35_Profile.jpg" class="img-responsive center-block" />
+                    <img title="Neha during her arangetram" alt="Neha during her arangetram" src="images/Profile/Neha600_35_Profile.jpg" class="img-responsive floatleft" />
                 </p>
-
+				<div class="center-block">
+                    <img title="Neha performing as Valli" alt="Neha performing as Valli" src="images/Profile/ProfilePage-AsValli.jpg" class="img-responsive floatleft" />
+                    <img title="Neha during her arangetram" alt="Neha during her arangetram" src="images/Profile/Neha600_22.jpg" class="img-responsive floatleft" />
+                </div>
                 </div>
             </div>
-					
+
 			</div>
 			<?php include("include/footer.php"); ?>
 	</body>
