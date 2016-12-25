c87c8e5c92d93b8756c3011024a40a2751308c49
Vani Thilagar <vthilagar@g.harvard.edu>
2016-12-25 09:50:55 -0500

    Layout and font change for the opening Arts page

--- TheArts.php
+++ TheArts.php
@@ -15,31 +15,28 @@
                     <li class="active"><a href="#ClassicalArt" role="tab" data-toggle="tab">Classical Art forms</a></li>
                     <li><a href="#Bharatanatyam" role="tab" data-toggle="tab">Bharatanatyam</a></li>
                     <li><a href="#Mohiniattam" role="tab" data-toggle="tab">Mohiniattam</a></li>
-                    <li><a href="#FolkDances" role="tab" data-toggle="tab">Folk Dances</a></li>					
+                    <li><a href="#FolkDances" role="tab" data-toggle="tab">Folk Dances</a></li>
                 </ul>
                 <div class="tab-content">
                     <div class="active tab-pane fade in" id="ClassicalArt">
                         <h2 class="text-info text-center">The Classical Art Forms of India</h2>
 						<div class="row">
-							<div class="col-md-4">
+							<div class="col-md-6">
 								<p>
-								<img title="Sculpture of dance and music" alt="Sculpture of dance and music" src="images/dancemusic.gif" class="img-responsive" style="float:left; margin-right:10px;"/></p>
+								<img title="Sculpture of dance and music" alt="Sculpture of dance and music" src="images/dancemusic.gif" class="img-responsive center-block" /></p>
+<p> <img title="Dancer displeased with lover" alt="Dancer displeased with lover" src="images/IMG_1188_2web.jpg" class="img-responsive center-block" /></p>
 							</div>
-							<div class="col-md-4">
-								<p>
+							<div class="col-md-6">
+								<h4>
 								The aim of all Indian art is to produce aesthetic enjoyment or 'rasa' in the spectator. <br />
 								According to the 'Natya Sastra', the Indian text of dance and drama dating back to approximately 200 B.C., a dancer must prepare himself/herself mentally and then employ certain techniques in order to succeed in producing rasa.<br />
-								</p>
-								<p>
+<br />
 								These dance forms are comprised of three aspects, 'Nritta' - pure dance, 'Abhinaya' - expressive mime and 'Nritya' - a combination of both Nritta and Abhinaya.<br />
-								</p>
-								<p>
-									All Indian dances are religious in thematic content as their origin is from mythology and epics. Thus the storiesPoses portrayed in these dances, are generally associated with the Gods of the Hindu pantheon and give scope for dramatic presentation.
-								</p>
+<br />
+									All Indian dances are religious in thematic content as their origin is from mythology and epics. Thus the stories and poses portrayed in these dances, are generally associated with the Gods of the Hindu pantheon and give scope for dramatic presentation.
+								</h4>
 							</div>
-							<div class="col-md-4">
-							<p> <img title="Dancer displeased with lover" alt="Dancer displeased with lover" src="images/IMG_1188_2web.jpg" class="img-responsive" style="float:right; margin-left:10px;"/></p>
-							</div>
+
 						</div>
                     </div>
                     <div class="tab-pane fade in" id="Bharatanatyam">
@@ -52,24 +49,24 @@
                             <br />
                             <img title="Bharatanatyam Dancer" alt="Bharatanatyam dancer" src="images/IMG_7226_copy.jpg" class="img-responsive" style="float:right; margin-left:10px;"/>Bharata Natyam style is an exalted one, circumscribed by meticulous rules governing style and technique.Though this art form is serene and tranquil in nature, Bharata Natyam is notable for it's complex rhythmic patterns, the clear geometries made by the dancer's body and the geometries within the performance space. <br />
                             The Abhinaya of traditional Bharata Natyam revolves around a heroine in a state of anticipation of either separation or union with her beloved. As this woman anticipating a union with her lover is also a metaphor for the human-being waiting for a union with the Unseen, it can be interpreted in either a religious or secular frame work. <br />
-                            The combination and subtleties of these movements and themes, their close connections with Carnatic music and the vast realm of Indian mythology, give Bharata Natyam a rare union of complexity and accessibility which are key to art of exquisite beauty.                        
+                            The combination and subtleties of these movements and themes, their close connections with Carnatic music and the vast realm of Indian mythology, give Bharata Natyam a rare union of complexity and accessibility which are key to art of exquisite beauty.
                         </p>
 
                     </div>
                     <div class="tab-pane fade in" id="Mohiniattam">
                         <h2 class="text-info text-center">Mohiniattam</h2>
                         <p>
-                            <img title="Neha performing Mohiniattam" alt="Neha performing Mohiniattam" src="images/NehaMohiniAttam_03_WM.jpg" style="float:left; margin-right:10px;"/>Mohiniyattam, also spelt Mohiniattam  is a traditional South Indian dance from Kerala, developed by the Tamil nattuvanar (dance master) Vadivelu,one of the Thanjavur Quartet.<br /> 
-                            It is a feminine dance form. The term Mohiniattam comes from the words "Mohini" meaning a woman who enchants onlookers and "aattam" meaning graceful and sensuous body movements.<br />  
-                            The word "Mohiniattam" literally means "dance of the enchantress". This dance which has influences and elements from two South Indian dance forms, the Bharatanatyam and Kathakali, was formulated in the court of King Swati Tirunal by Vadivelu.<br />  
-                            <img title="Mohiniattam pose"  alt="Mohiniattam pose" src="images/MohiniattamAtKresge2.jpg" style="float:right; margin-left:10px;"/>Here the movements of the dancer's body could be compared to the gently undulating palm fronds filling up the Kerala skyline.<br />  
-                            There is a lot of heaving of the chest and swaying movements like the coconut palms. The gait and the stepping are reminiscent of a boat bobbing up and down in the gently soaring back waters of the kerala landscape, the land of Mohiniattam.<br />                         
+                            <img title="Neha performing Mohiniattam" alt="Neha performing Mohiniattam" src="images/NehaMohiniAttam_03_WM.jpg" style="float:left; margin-right:10px;"/>Mohiniyattam, also spelt Mohiniattam  is a traditional South Indian dance from Kerala, developed by the Tamil nattuvanar (dance master) Vadivelu,one of the Thanjavur Quartet.<br />
+                            It is a feminine dance form. The term Mohiniattam comes from the words "Mohini" meaning a woman who enchants onlookers and "aattam" meaning graceful and sensuous body movements.<br />
+                            The word "Mohiniattam" literally means "dance of the enchantress". This dance which has influences and elements from two South Indian dance forms, the Bharatanatyam and Kathakali, was formulated in the court of King Swati Tirunal by Vadivelu.<br />
+                            <img title="Mohiniattam pose"  alt="Mohiniattam pose" src="images/MohiniattamAtKresge2.jpg" style="float:right; margin-left:10px;"/>Here the movements of the dancer's body could be compared to the gently undulating palm fronds filling up the Kerala skyline.<br />
+                            There is a lot of heaving of the chest and swaying movements like the coconut palms. The gait and the stepping are reminiscent of a boat bobbing up and down in the gently soaring back waters of the kerala landscape, the land of Mohiniattam.<br />
                         </p>
                     </div>
 					<div class="tab-pane fade in" id="FolkDances">
                         <h2 class="text-info text-center">Folk Dances of India</h2>
                         <p>
-                            <img title="Laavani Folk dance of Maharashtra" alt="Laavani Folk dance of Maharashtra" src="images/Folkdances/Laavani.jpg" style="float:left; margin-right:10px;"/>Folk dance in India is a term broadly used to describe all forms of folk and tribal dances in regions across India. While Indian classical dance is considered a higher form of art and was practiced in courts, temples and on special occasions, folk dance forms are practiced in groups in rural areas as an expression of their daily work and rituals. They are performed on religious , seasonal festivals or entertainment. <br/>They reflect the traditional life and  cultural flavor of the life of the people of a certain country or region. Some of the popular folk dances that are performed across villages of India are are Bhangra, Garba, Ghummur, Laavni, Tamasha,  Bihu, and so on<br /><br />                        
+                            <img title="Laavani Folk dance of Maharashtra" alt="Laavani Folk dance of Maharashtra" src="images/Folkdances/Laavani.jpg" style="float:left; margin-right:10px;"/>Folk dance in India is a term broadly used to describe all forms of folk and tribal dances in regions across India. While Indian classical dance is considered a higher form of art and was practiced in courts, temples and on special occasions, folk dance forms are practiced in groups in rural areas as an expression of their daily work and rituals. They are performed on religious , seasonal festivals or entertainment. <br/>They reflect the traditional life and  cultural flavor of the life of the people of a certain country or region. Some of the popular folk dances that are performed across villages of India are are Bhangra, Garba, Ghummur, Laavni, Tamasha,  Bihu, and so on<br /><br />
                         </p>
                     </div>
                 </div>
