6378e5df651ea902469bbfe6aee3b913c9e60b16
Vani Thilagar <vthilagar@g.harvard.edu>
2016-12-25 19:52:08 -0500

    Updated the awards section with the latest images

--- Awards.php
+++ Awards.php
@@ -19,53 +19,142 @@
                         <li data-target="#my-awards" data-slide-to="3"></li>
                         <li data-target="#my-awards" data-slide-to="4"></li>
 						<li data-target="#my-awards" data-slide-to="5"></li>
+						<li data-target="#my-awards" data-slide-to="6"></li>
+                        <li data-target="#my-awards" data-slide-to="7"></li>
+                        <li data-target="#my-awards" data-slide-to="8"></li>
+                        <li data-target="#my-awards" data-slide-to="9"></li>
+						<li data-target="#my-awards" data-slide-to="10"></li>
+						<li data-target="#my-awards" data-slide-to="11"></li>
+						<li data-target="#my-awards" data-slide-to="12"></li>
+                        <li data-target="#my-awards" data-slide-to="13"></li>
+                        <li data-target="#my-awards" data-slide-to="14"></li>
+						<li data-target="#my-awards" data-slide-to="15"></li>
+						<li data-target="#my-awards" data-slide-to="16"></li>
                     </ol>
-                    <div class="carousel-inner">
-                        <div class="item active">
+                    <div class="carousel-inner" id="awards_slider">
+                        <div class="item active" id="0">
+							<div class="carousel-caption">
+								<h3>1st Place Winners</h3>
+								<h4>2015 NEMA Drishya</h4>
+							</div>
                             <img src="images/Awards/2015-NEMA-Drishya-1stPlaceWinnersSeniorCategory.jpg" alt="2015 NEMA Drishya 1st Place Winners" title="2015 NEMA Drishya 1st Place Winners" class="center-block" />
-                            <div class="carousel-caption">
-                                <h3>1st Place Winners</h3>
-                                <h4>2015 NEMA Drishya</h4>
-                            </div>
                         </div>
-                        <div class="item">
+                        <div class="item" id="1">
                             <img src="images/Awards/2015-IANH-VasantUtsav-2ndPlaceWinnerShrijaaMohanClassicalDanceSolo.jpg" alt="2015 IANH Vasant Utsav 2nd Place Winner Shrijaa Mohan Classical Dance Solo" title="2015 IANH Vasant Utsav 2nd Place Winner Shrijaa Mohan Classical Dance Solo "class="center-block" />
                             <div class="carousel-caption">
                                 <h3>2nd Place Winner: Shrijaa Mohan</h3>
                                 <h4>2015 IANH Vasant Utsav - Classical Dance Solo</h4>
                             </div>
                         </div>
-                        <div class="item">
+                        <div class="item" id="2">
+							<div class="carousel-caption">
+								<h3>2nd Place Winners</h3>
+								<h4>2015 IANH Vasant Utsav - Senior Category</h4>
+							</div>
                             <img src="images/Awards/2015-IANH-VasantUtsav-2ndPlaceWinnersSeniorCategory.jpg"  alt="2015 IANH Vasant Utsav 2nd Place Winners Senior Category" title="2015 IANH Vasant Utsav 2nd Place Winners Senior Category" class="center-block" />
-                            <div class="carousel-caption">
-                                <h3>2nd Place Winners</h3>
-                                <h4>2015 IANH Vasant Utsav - Senior Category</h4>
-                            </div>
+
                         </div>
-						<div class="item">
+						<div class="item" id="3">
                             <img src="images/Awards/2015-IANH-VasantUtsave2ndPlaceWinnersGroupClassicalDance.jpg"  alt="2015 IANH Vasant Utsav 2nd Place Winners Group Classical" title="2015 IANH Vasant Utsav 2nd Place Winners Group Classical" class="center-block" />
                             <div class="carousel-caption">
                                 <h3>2nd Place Winners</h3>
                                 <h4>2015 IANH Vasant Utsav - Group Classical</h4>
                             </div>
                         </div>
-                        <div class="item">
+                        <div class="item" id="4">
                             <img src="images/Awards/2014-Swaraag1stPlaceWinnerSoloShrvani.jpg" alt="Shrvani Balaji" title="2014 Swaraag First Place Winner Shravani Balaji" class="center-block" />
                             <div class="carousel-caption">
                                 <h3>2014 Swaraag First Place Winner</h3>
-                                <p>Shravani Balaji</p>
+                                <h4>Shravani Balaji</h4>
                             </div>
                         </div>
-                        <div class="item">
+                        <div class="item" id="5">
                             <img src="images/Awards/2014-Swaraag2ndPlaceWinnersJuniorCategory.jpg" alt="2014-Swaraag 2nd Place Winners Junior Category" title="2014-Swaraag 2nd Place Winners Junior Category" class="center-block" />
                             <div class="carousel-caption">
                                 <h3>2nd Place Winners</h3>
-                                <p>2014-Swaraag Junior Category</p>
+                                <h4>2014-Swaraag Junior Category</h4>
+                            </div>
+                        </div>
+						<div class="item" id="6">
+                            <img src="images/Awards/2014-NEMA-1stPlaceWinnersSeniorCategory.jpg" alt="2014-NEMA 1st Place Winners Senior Category" title="2014-NEMA 1st Place Winners Senior Category" class="center-block" />
+                            <div class="carousel-caption">
+                                <h3>1st Place Winners</h3>
+                                <h4>2014-NEMA Senior Category</h4>
+                            </div>
+                        </div>
+						<div class="item" id="7">
+                            <img src="images/Awards/2014-IANH-SpringFestival-3rdPlaceWinnersJuniors.jpeg" alt="2014-IANH-SpringFestival-3rdPlaceWinnersJuniors" title="2014-IANH-SpringFestival-3rdPlaceWinnersJuniors" class="center-block" />
+                            <div class="carousel-caption">
+                                <h3>3rd Place Winners Juniors</h3>
+                                <h4>2014-IANH-SpringFestival</h4>
+                            </div>
+                        </div>
+						<div class="item" id="8">
+                            <img src="images/Awards/2014-IANH-SpringFestival-2ndPlace WinnersJuniors.jpeg" alt="2014 IANH SpringFestival 2ndPlace Winners Juniors" title="2014 IANH SpringFestival 2ndPlace Winners Juniors" class="center-block" />
+                            <div class="carousel-caption">
+                                <h3>2ndPlace Winners Juniors</h3>
+                                <h4>2014 IANH SpringFestival</h4>
+                            </div>
+                        </div>
+						<div class="item" id="9">
+                            <img src="images/Awards/2014-IANH-SpringFestival1stPlaceWinnersSenior.jpeg" alt="2014-IANH-SpringFestival 1st Place Winners Senior" title="2014-IANH-SpringFestival 1st Place Winners Senior" class="center-block" />
+                            <div class="carousel-caption">
+                                <h3>1st Place Winners</h3>
+                                <h4>2014-IANH-SpringFestival Senior</h4>
+                            </div>
+                        </div>
+						<div class="item" id="10">
+                            <img src="images/Awards/2013-Drishya-NEMA-1stPlaceWinnersSeniorCategory.jpg" alt="2013-Drishya-NEMA 1st Place Winners Senior Category" title="2013-Drishya-NEMA 1st Place Winners Senior Category" class="center-block" />
+                            <div class="carousel-caption">
+                                <h3>1st Place Winners</h3>
+                                <h4>2013-Drishya-NEMA Senior Category</h4>
+                            </div>
+                        </div>
+						<div class="item" id="11">
+                            <img src="images/Awards/2013-Drishya2ndPlace WinnersGroupClassicalJuniorCategory.jpg" alt="2013-Drishya 2nd Place Winners Group Classical Junior Category" title="2013-Drishya 2nd Place Winners Group Classical Junior Category" class="center-block" />
+                            <div class="carousel-caption">
+                                <h3>2nd Place Winners</h3>
+                                <h4>2013-Drishya Group Classical Junior Category</h4>
+                            </div>
+                        </div>
+						<div class="item" id="12">
+                            <img src="images/Awards/2013-BostonSargam-2ndPlaceWinner-SoloShrvani.jpg" alt="2013 Boston Sargam 2ndPlaceWinner Solo Shrvani" title="2013 Boston Sargam 2ndPlaceWinner Solo Shrvani" class="center-block" />
+                            <div class="carousel-caption">
+                                <h3>2nd PlaceWinner Solo Shrvani</h3>
+                                <h4>2013 Boston Sargam</h4>
+                            </div>
+                        </div>
+						<div class="item" id="13">
+                            <img src="images/Awards/2012-BostonSargam2ndPlaceWinnersSeniorCategory.jpg" alt="2012 Boston Sargam 2nd Place Winners Senior Category" title="2012 Boston Sargam 2nd Place Winners Senior Category" class="center-block" />
+                            <div class="carousel-caption">
+                                <h3>2nd Place Winners</h3>
+                                <h4>2012 Boston Sargam Senior Category</h4>
+                            </div>
+                        </div>
+						<div class="item" id="14">
+                            <img src="images/Awards/2012_Nema_FirstPlace-Senior_Mountain Background pic.jpg" alt="2012 Nema First Place Senior" title="2012 Nema First Place Senior" class="center-block" />
+                            <div class="carousel-caption">
+                                <h3>First Place Senior</h3>
+                                <h4>2012 NEMA</h4>
+                            </div>
+                        </div>
+						<div class="item" id="15">
+                            <img src="images/Awards/2011-1stPlaceWinnerAtBostonSargam2011SoloShrvani.jpg" alt="2011-1st Place Winner At Boston Sargam Solo Shrvani" title="2011-1st Place Winner At Boston Sargam Solo Shrvani" class="center-block" />
+                            <div class="carousel-caption">
+                                <h3>1st Place Winner</h3>
+                                <h4>2011 Boston Sargam Solo- Shrvani</h4>
+                            </div>
+                        </div>
+						<div class="item" id="16">
+                            <img src="images/Awards/2008_Kalotsav-2ndPlaceJuniorCategory.gif" alt="2008 Kalotsav 2nd Place Junior Category" title="2008 Kalotsav 2nd Place Junior Category" class="center-block" />
+                            <div class="carousel-caption">
+                                <h3>2nd Place Winners</h3>
+                                <h4>2008 Kalotsav-Junior Category</h4>
                             </div>
                         </div>
                      </div>
                     <a class="left carousel-control" href="#my-awards" data-slide="prev">
-                        <span class="glyphicon glyphicon-chevron-left"> </span> 
+                        <span class="glyphicon glyphicon-chevron-left"> </span>
                     </a>
                     <a class="right carousel-control" href="#my-awards" data-slide="next">
                          <span class="glyphicon glyphicon-chevron-right"></span>
--- TheAcademy.php
+++ TheAcademy.php
@@ -11,15 +11,15 @@
 					<div id="main" class="row">
 
             <div class="col-md-8 col-md-offset-2"><h3 class="text-info text-center">About the Academy</h3>
-                <img title="Students performing" alt="Students performing" src="images/Laconia_02.JPG" class="img-responsive center-block"><p>Neha founded the Chidambaram Nrityakala Academy in early 2005. Here she inspires all her students to strive for perfection and the individual focused training brings out the best in them. She is very popular in the Nashua area for her authentic style of teaching and uncompromising discipline that helps her students reach the highest standards in dance.</p>  
+                <img title="Students performing" alt="Students performing" src="images/Laconia_02.JPG" class="img-responsive center-block"><p>Neha founded the Chidambaram Nrityakala Academy in early 2005. Here she inspires all her students to strive for perfection and the individual focused training brings out the best in them. She is very popular in the Nashua area for her authentic style of teaching and uncompromising discipline that helps her students reach the highest standards in dance.</p>
 				<p>While she specializes in teaching Bharatanatyam and Mohiniattam she is very knowledgeable and adept with the Folk dances of India. She also teaches the popular Bollywood style dances.</p>
 				<p>Her students are primarily in the school age range, but she also enjoys teaching the classical and Bollywood style dances to adults. <br />
-                    <img alt="Shravani won 1st place in Bostong Sargam event" style="float:right; margin-left:4px;" title="Winner of Boston Sargam event" src="images/Carousel/ShravaniSargamWM.png" />She has trained and choreographed many performances for her students for dance festivals, dance competitions, fund raising events all around the New England area and some of her students have also performed in India as well. Her students won various dance competitions in the New England area.</p> 
+                She has trained and choreographed many performances for her students for dance festivals, dance competitions, fund raising events all around the New England area and some of her students have also performed in India as well. Her students won various dance competitions in the New England area.</p>
 				<p>Following the Guru-Shishya traditions in teaching, she interacts with students in trying to get the best out of every student. It is to her credit that five of her students have performed their 'Arangetram' or solo dance debut recitals since 2006 and more are on their way to achieving this milestone.</p>
-				<p>
-				<h4 class="text-info text-center">Why Learn Classical Indian Dance !!!!!!</h4>
+				<br />
+				<h4 class="text-info text-center">Why Learn Classical Indian Dance?</h4>
 				<ol>
-					<li>Learning classical dances makes you a BEAUTIFUL , GRACEFUL and ELEGANT person inside out. It helps tone your body and gives you a 
+					<li>Learning classical dances makes you a BEAUTIFUL , GRACEFUL and ELEGANT person inside out. It helps tone your body and gives you a
 					beautiful posture and a healthy glowing skin. It enchances your facial features as well.</li>
 					<li>Improves flexibility and stamina.</li>
 					<li>Boosts your physical endurance, self confidence and self esteem.</li>
@@ -31,7 +31,7 @@
 					<li>A deeper connection witht the Indian cultural roots and an insight into the rich Indian myhtology and the Hindu religion.</li>
 					<li>Classical Indian dance and traditional Indian culture has such great DEPTH, that it can bring about a great change in your child's development at school, other actiivites and over all personality.</li>
 				</ol>
-				</p>
+
             </div>
 			<div class="col-md-2">
 			<h4><a href="docs/DanceSchoolRules.pdf">Download a registration form</a></h4>
